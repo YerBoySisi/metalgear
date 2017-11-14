d6f441979b097d79f1a6b6a594ac282c38c48323
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-11-14 11:26:07 -0500

    Lvl and Olvl implemented

---
+++ Metal Gear/src/explorer/tempMain.java
@@ -0,0 +1,96 @@
+package explorer;
+
+public class tempMain {
+	static int[][] lvl;
+	static Thing[][] olvl;
+	
+	public static void main(String[] args) {
+		
+		lvl = setLevel1();
+		convertLevel();
+		displayOLevel();
+	}
+
+	public static int[][] setLevel1() {
+		
+		//blank = 0 
+		//wall = 1
+
+		int[][] temp= {
+				{1,1,1,1,1,1,1,1,1,1},
+				{1,0,0,1,0,0,0,0,0,1},
+				{1,0,0,1,0,0,0,0,0,1},
+				{1,0,0,1,0,0,1,0,0,1},
+				{1,0,1,1,0,0,1,0,0,1},
+				{1,0,0,0,0,0,1,0,0,1},
+				{1,0,0,0,0,0,1,0,0,1},
+				{1,0,0,1,1,1,1,0,0,1},
+				{1,0,0,0,0,0,0,0,0,1},
+				{1,1,1,1,1,1,1,1,1,1}
+		};
+		
+		return temp;
+	}
+	
+	public static void convertLevel() {
+		int temp = 0;
+		
+		olvl = new Thing[lvl.length][lvl[0].length];
+		for(int i = 0; i < lvl.length; i++) {
+			for(int j = 0; j< lvl[0].length; j++) {
+				temp = lvl[i][j];
+				
+				if(temp == 0) {
+					olvl[i][j] = new Thing(j,i);
+				}else {
+					olvl[i][j] = new Wall(j,i);
+				}
+				
+			}
+		}
+		
+	}
+	
+	public static void displayOLevel() {
+		Thing temp;
+		
+		for(int i = 0; i < lvl.length; i++) {
+			for(int j = 0; j< lvl[0].length; j++) {
+				temp = olvl[i][j];
+				
+				if(temp instanceof Wall) {
+					System.out.print("■");
+				}else{
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	
+	
+	
+	public static void displayLevel() {
+		int temp = 0;
+		
+		for(int i = 0; i < lvl.length; i++) {
+			for(int j = 0; j< lvl[0].length; j++) {
+				temp = lvl[i][j];
+				
+				if(temp == 0) {
+					System.out.print(" ");
+				}else {
+					System.out.print("■");
+				}
+				
+			}
+			System.out.print("\n");
+		}
+		
+	}
+	
+	
+
+}

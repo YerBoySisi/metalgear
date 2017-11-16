81b84b5b4c520ee3afd53967b37c76414b9ec857
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-11-16 11:44:39 -0500

    Field of view almost complete

--- Metal Gear/src/entity/Guard.java
+++ Metal Gear/src/entity/Guard.java
@@ -2,10 +2,6 @@ package entity;
 
 import java.util.Arrays;
 
-import caves.CaveRoom;
-import caves.NPCRoom;
-import explorer.ExplorerMain;
-
 public class Guard extends NPC implements Person {
 	
 	//constants
@@ -13,14 +9,14 @@ public class Guard extends NPC implements Person {
 	public static final int EAST = 1;
 	public static final int SOUTH = 2;
 	public static final int WEST = 3;
-	public static final int[] directions = {-1, -1, 1, 1};
+	public static final int[] directions = {-1, 1, 1, -1};
 	
 	//fields relating to navigation
 	private int currentRow;
 	private int currentCol;
 	private int[][] path;
 	private int direction;
-	private int currentPos = 0;
+	private int currentPos;
 	
 	//fields relating to character
 	private String name;
@@ -45,6 +41,9 @@ public class Guard extends NPC implements Person {
 			
 		}
 		
+		this.currentPos = path.length - 1;
+		setDirection();
+		setFieldOfView();
 		this.active = true;
 
 	}
@@ -65,7 +64,11 @@ public class Guard extends NPC implements Person {
 		
 		if(active) {
 			System.out.println(Arrays.deepToString(path));
+			System.out.println(Arrays.deepToString(fieldOfView));
+			System.out.println(direction);
+			System.out.println(currentPos);
 			move(currentPos);
+			setDirection();
 			setFieldOfView();
 			currentPos++;
 			
@@ -91,32 +94,68 @@ public class Guard extends NPC implements Person {
 	
 	public void setDirection() {
 		
-		if(path[currentPos][0] < path[currentPos - 1][0]) {
-			direction = NORTH;
-		} else if(path[currentPos][0] > path[currentPos - 1][0]) {
-			direction = SOUTH;
-		} else if(path[currentPos][0] == path[currentPos - 1][0]) {
-			direction = (int)(Math.random() * 4);
-		}
+		if(currentPos > 0) {
+			
+			if(currentRow < path[currentPos - 1][0]) {
+				direction = NORTH;
+			} else if(currentRow > path[currentPos - 1][0]) {
+				direction = SOUTH;
+			} else if(currentRow == path[currentPos - 1][0]) {
+				direction = (int)(Math.random() * 4);
+			} else if(currentCol < path[currentPos - 1][1]) {
+				direction = WEST;
+			} else if(currentCol > path[currentPos - 1][1]) {
+				direction = EAST;
+			} else if(currentCol == path[currentPos - 1][1]) {
+				direction = (int)(Math.random() * 4);
+			}
 		
-		if(path[currentPos][1] < path[currentPos - 1][1]) {
-			direction = WEST;
-		} else if(path[currentPos][1] > path[currentPos - 1][1]) {
-			direction = EAST;
-		} else if(path[currentPos][1] == path[currentPos - 1][1]) {
-			direction = (int)(Math.random() * 4);
-		} 
+		} else {
+			
+			if(path[currentPos][0] > path[currentPos + 1][0]) {
+				direction = NORTH;
+			} else if(path[currentPos][0] < path[currentPos + 1][0]) {
+				direction = SOUTH;
+			} else if(path[currentPos][0] == path[currentPos + 1][0]) {
+				direction = (int)(Math.random() * 4);
+			} else if(path[currentPos][1] > path[currentPos + 1][1]) {
+				direction = WEST;
+			} else if(path[currentPos][1] < path[currentPos + 1][1]) {
+				direction = EAST;
+			} else if(path[currentPos][1] == path[currentPos + 1][1]) {
+				direction = (int)(Math.random() * 4);
+			}
+			
+		}
 		
 	}
 	
 	public void setFieldOfView() {
 		
-		fieldOfView = new int[6][2];
+		fieldOfView = new int[4][2];
 		
-		for(int i = 0; i < fieldOfView.length; i++) {
-			fieldOfView[i][0] = path[i][0] + directions[direction];
+		if(direction == NORTH || direction == SOUTH) {
+			
+			fieldOfView[0][0] = currentRow + directions[direction];
+			fieldOfView[1][0] = fieldOfView[0][0] + directions[direction];
+			fieldOfView[2][0] = fieldOfView[1][0];
+			fieldOfView[3][0] = fieldOfView[1][0];
+			fieldOfView[2][1] = currentCol + 1;
+			fieldOfView[3][1] = currentCol - 1;
+			
+		
 		}
 		
+		if(direction == EAST || direction == WEST) {
+			
+			fieldOfView[0][1] = currentCol + directions[direction];
+			fieldOfView[1][1] = fieldOfView[0][1] + directions[direction];
+			fieldOfView[2][1] = fieldOfView[1][1];
+			fieldOfView[3][1] = fieldOfView[1][1];
+			fieldOfView[2][0] = currentRow + 1;
+			fieldOfView[3][0] = currentRow - 1;
+		
+		}
 		
 	}
 
